9d576af073626ecc637fc2a068b60aaee3603da3
IbadUrRehman143 <ibadurrehman010@gmail.com>
2024-08-29 01:25:01 +0500

    pp

--- Login_app/src/Update.jsx
+++ Login_app/src/Update.jsx
@@ -29,25 +29,48 @@ const Update = () => {
     }, [])
 
     return (
-        <div className='d-flex justify-content-center align-items-center bg-secondary vh-100'>
-           <div className=' bg-white p-3 rounded w-25'>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="email"><strong>Name</strong></label>
-                        <input type="text" placeholder='Enter Name ' name='email' className='form-control rounded-0' value={name} onChange={(e) => setName(e.target.value)} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email" name="email" placeholder='Enter Email' autoComplete="off" className='form-control rounded-0' value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email"><strong>Password</strong></label>
-                        <input type="password" name="password" placeholder="Enter password" className='form-control rounded-0' value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <button type='submit' className='btn btn-success w-100 rounded-0 '>Update</button>
-                </form>
-           </div>
+        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+        <div className="bg-white p-3 rounded w-100 w-sm-75 w-md-50 w-lg-25">
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="name"><strong>Name</strong></label>
+                    <input 
+                        type="text" 
+                        placeholder="Enter Name" 
+                        name="name" 
+                        className="form-control rounded-0" 
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)} 
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email"><strong>Email</strong></label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Enter Email" 
+                        autoComplete="off" 
+                        className="form-control rounded-0" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password"><strong>Password</strong></label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Enter Password" 
+                        className="form-control rounded-0" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                </div>
+                <button type="submit" className="btn btn-success w-100 rounded-0">Update</button>
+            </form>
         </div>
+    </div>
+    
     )
 }
 
